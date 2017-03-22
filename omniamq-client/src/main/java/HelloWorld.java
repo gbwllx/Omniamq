0b1ebca1b7ab8339c19f10955402f1226a3f36f3
muming <gubing.gb@alibaba-inc.com>
2017-03-22 14:41:31 +0800

    这是创世纪的大事件

---
+++ omniamq-client/src/main/java/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * description:
+ * author: gubing.gb
+ * date: 2017/3/22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello");
+    }
+}
